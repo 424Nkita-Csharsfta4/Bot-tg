983a3ecd04de444b869e8b7a48f5399423ff2e2b
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2022-12-11 12:58:38 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+export const PORT = 3000
+export const TOKEN = '5640366062:AAHPjgd_nxh6aRkymwbhnLE5-JYZaaBahiU'
---
+++ index.js
@@ -0,0 +1,9 @@
+//const Telegraf = require('telegraf')
+//const Extra = require('telegraf/extra')
+//const fs = require('fs')
+export const PORT = 3000
+export const TOKEN = '5640366062:AAHPjgd_nxh6aRkymwbhnLE5-JYZaaBahiU'
+
+
+const bot = new Telegraf("5640366062:AAHPjgd_nxh6aRkymwbhnLE5-JYZaaBahiU");
+
